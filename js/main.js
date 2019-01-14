b0f43f017d625f772000ae2706b64d229823d3a6
Yunus <b_yunus@tss-hr420-dm37.fc.ca>
2019-01-14 16:22:24 -0500

    java added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(() => {
+	// self-invoking anonymous function (or Lambda)
+	// var = create variable
+	// whatever comes after the = is what the variable ref
+	var images = document.querySelectorAll('img');
+
+	// document is always the webpage
+	// after the dot is a mothod or a property
+
+	console.log(images);
+	
+})();
